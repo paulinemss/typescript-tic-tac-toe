e45a3ca7ed86fb7d16177a1863f07bfb7c2afbd0
paulinemss <masse.pauline@gmail.com>
2021-01-13 15:09:14 +0100

    Finalized tictactoe exercise

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,107 @@ const boardElement = document.getElementById("board");
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
-let boardState = [
+type Cell = "X" | "O" | ""; 
+type TicTacToeBoard = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell]
+]
+
+let boardState: TicTacToeBoard = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
 ];
-let currentMove = "X";
+let currentMove: "X" | "O" = "X";
+let winner: Cell | "Draw" = ""; 
 
-function createCell(row, col, content = "") {
+function createCell(row: number, col: number, content: Cell = "") {
   const cell = document.createElement("button");
   cell.setAttribute("data-row", row.toString());
   cell.setAttribute("data-col", col.toString());
   cell.setAttribute("data-content", content);
   cell.classList.add("cell");
+  cell.addEventListener("click", () => {
+    if (winner) return; 
+    if (boardState[row][col] === "") {
+      boardState[row][col] = currentMove; 
+      currentMove = currentMove === "X" ? "O" : "X"
+      winner = checkBoard()
+      renderBoard()
+    }
+  })
   return cell;
+}
+
+type Coordinate = [number, number];
+type Victory = [Coordinate, Coordinate, Coordinate]; 
+
+const victories: Victory[] = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
+
+function checkBoard(): Cell | "Draw" {
+
+  // checking for victories
+  for (let victory of victories) {
+    const cell1 = boardState[victory[0][0]][victory[0][1]];
+    const cell2 = boardState[victory[1][0]][victory[1][1]];
+    const cell3 = boardState[victory[2][0]][victory[2][1]];
+    if (cell1 !== "" && cell1 === cell2 && cell2 === cell3) {
+      return cell1;
+    }
+  }
+
+  // checking if there is a draw 
+  let isDraw = true; 
+  for (let i=0; i < ROW_COUNT; i++) {
+    for (let j=0; j < COL_COUNT; j++) {
+      if (boardState[i][j] === "") isDraw = false;
+    }
+  }
+  if (isDraw) return "Draw";
+
+  return ""; 
 }
 
 function renderBoard() {
@@ -36,7 +123,7 @@ function renderBoard() {
   }
   const moveElement = document.createElement("p");
   moveElement.id = "move-element";
-  moveElement.innerText = `Next Move: ${currentMove}`;
+  moveElement.innerText = winner ? `Winner: ${winner}` : `Next Move: ${currentMove}`;
   moveElement.classList.add("current-move");
   appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
@@ -51,6 +138,7 @@ function init() {
       ["", "", ""]
     ];
     currentMove = "X";
+    winner = ""; 
     renderBoard();
   });
   renderBoard();
